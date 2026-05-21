6758a07554cce87309d5d0f300097dcb8f6a2bac
KimberlyZheng <kimberlyz26@nycstudents.net>
2026-05-21 08:46:20 -0400

    part 3

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,5 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,14 +9,34 @@ public class Card {
     private String value;
     private String imageFileName;
     private BufferedImage image;
+    private Rectangle hitbox;
+    private boolean visible;
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
 
     public Card(String suit, String value) {
         this.suit = suit;
         this.value = value;
         this.imageFileName = "card_"+suit+"_"+value+".png";
         this.image = readImage();
+        this.hitbox = new Rectangle(-10, -10, image.getWidth(), image.getHeight());
+        this.visible = true;
     }
 
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+
+    public void setHitbox(Rectangle hitbox) {
+        this.hitbox = hitbox;
+    }
 
     public String getSuit() {
         return suit;
--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -32,19 +32,37 @@ class DrawPanel extends JPanel implements MouseListener {
         for (int row = 0; row < onScreen.length; row++) {
             for (int col = 0; col < onScreen[0].length; col++) {
                 g.drawImage(onScreen[row][col].getImage(), x + (100*col), y + (100*row), null);
+                Rectangle hitbox = new Rectangle(x + (100* col), y + (100*row), onScreen[row][col].getImage().getWidth(),
+                        onScreen[row][col].getImage().getHeight());
+                onScreen[row][col].setHitbox(hitbox);
+                if (onScreen[row][col].isVisible()) {
+                    g.drawRect(x + (100 * col), y + (100 * row), (int) onScreen[row][col].getHitbox().getWidth(),
+                            (int) onScreen[row][col].getHitbox().getHeight());
+                }
             }
         }
         g.drawString("There are " + deck.getCards().size() + " cards left", 50, 300);
     }
 
     public void mousePressed(MouseEvent e) {
-        if (deck.getCards().size() >= 9){
+
+        Point p = e.getPoint();
+        int button = e.getButton();
+        //1 is left click, 3 is right click
+
         for (int row = 0; row < onScreen.length; row++) {
             for (int col = 0; col < onScreen[0].length; col++) {
-                currCard = deck.getRandomCard();
-                onScreen[row][col] = currCard;
+                if (onScreen[row][col].getHitbox().contains(p)){
+
+                    if (button == 3){
+                        onScreen[row][col].setVisible(!onScreen[row][col].isVisible());
+                    }
+
+                    if (deck.getCards().size() != 0 && button == 1) {
+                        onScreen[row][col] = deck.getRandomCard();
+                    }
+                }
             }
-        }
         }
     }
 
